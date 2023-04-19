62eed23aed4263e308a6ac89f267ed6e6da6764b
Vu Tong <tonghoangvu@outlook.com>
2023-04-19 09:50:43 +0700

    dev: config Vite env vars loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 function App() {
-	return null
+	const APP_NAME = import.meta.env.VITE_APP_NAME
+	const APP_VERSION = import.meta.env.VITE_APP_VERSION
+	return (
+		<p>
+			{APP_NAME} {APP_VERSION} ({import.meta.env.MODE} build)
+		</p>
+	)
 }
 
 export default App
