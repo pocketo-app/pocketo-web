efd7f56d83835cefec15433347e82226db99f145
Vu Tong <tonghoangvu@outlook.com>
2023-04-23 14:27:35 +0700

    chore: design sticky header

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import Header from './layout/Header'
 import Main from './layout/Main'
 
 function App() {
+	const headerStyle = {
+		'background-color': 'white',
+		'border-bottom': '1px solid var(--color-neutral-tint2)',
+	}
 	return (
 		<>
-			<div style={{ 'background-color': 'white' }}>
+			<div class="sticky-top on-top-1 shadow" style={headerStyle}>
 				<Header />
 			</div>
 			<div>
